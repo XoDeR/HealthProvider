87b0ac8a4025090006fc6ed28e712d7ebbea5d50
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-05 23:03:58 +0200

    added user model and register and login routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+app.use(express.json());
+const userRoute = require("./routes/userRoute");
+app.use("/api/user", userRoute);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
