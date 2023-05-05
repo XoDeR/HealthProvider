04c074f8ba87e7dc262d28057c3f68bc880cc127
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-05 23:28:43 +0200

    Server: MongoDB connection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
 
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
@@ -9,4 +10,4 @@ app.use("/api/user", userRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started at port ${port}`));
+app.listen(port, () => console.log(`Server started at port ${port}.`));
