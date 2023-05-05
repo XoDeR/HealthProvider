b4f0484eca45ec2cf829b027fe69b58230697520
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-05 20:21:57 +0200

    Using bootstrap and AntD

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import { Button, Space } from "antd";
+
 function App() {
   return (
     <div className="App">
       <h1>Health Provider</h1>
+      <Space wrap>
+        <div className="p-5">
+          <Button type="primary">Primary Button</Button>
+        </div>
+
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </Space>
     </div>
   );
 }
