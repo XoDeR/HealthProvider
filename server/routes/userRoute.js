36ae18bad4639842d64d68fc1613502a313e2e90
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-06 14:36:26 +0200

    Server: added user register route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
   try {
-  } catch (error) {}
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res
+        .status(200)
+        .send({ message: "User already exists", success: false });
+    }
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(200).send({ message: "User created", success: true });
+  } catch (error) {
+    res.status(500).send({ message: "Error creating user", success: false });
+  }
 });
 
 router.post("/login", async (req, res) => {
