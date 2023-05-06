fa6f5481a7427e749c64b0d6c309589ccab31fb8
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-06 19:27:57 +0200

    Client: user registration

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { Form, Input, Button } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 function Register() {
-  const onFinish = (values) => {
-    console.log("Form values: ", values);
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/api/user/register", values);
+      if (response.data.success) {
+        toast.success(response.data.message);
+        toast("Redirecting to Login page");
+        navigate("/login");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong.");
+    }
   };
 
   return (
