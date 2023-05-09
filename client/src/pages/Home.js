3cddf75e2f2e53af9ab593c507bee1e6f815255e
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-09 18:11:22 +0200

    Layout component structure with css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import Layout from "../components/Layout";
 
 function Home() {
   const getData = async () => {
@@ -23,7 +24,11 @@ function Home() {
     getData();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <Layout>
+      <h1>Home</h1>
+    </Layout>
+  );
 }
 
 export default Home;
