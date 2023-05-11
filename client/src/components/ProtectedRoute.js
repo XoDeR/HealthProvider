9c022e651e4973c147c40fb8f2e4a6005776f752
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-11 18:37:11 +0200

    ProtectedRoute: if getUser is not successful, remove token from localStorage

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -25,10 +25,12 @@ function ProtectedRoute(props) {
       if (response.data.success) {
         dispatch(setUser(response.data.data));
       } else {
+        localStorage.removeItem("token");
         navigate("/login");
       }
     } catch (error) {
       dispatch(hideLoading());
+      localStorage.removeItem("token");
       navigate("/login");
     }
   };
