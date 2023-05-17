e4ac6fa474c78819c35aeeffcfa84cf3f7a5a37a
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-17 23:01:25 +0200

    Client: notifications page

--- client/src/pages/Notifications.js
+++ client/src/pages/Notifications.js
@@ -1,10 +1,37 @@
 import React from "react";
 import Layout from "../components/Layout";
 import { Tabs } from "antd";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { showLoading, hideLoading } from "../redux/alertsSlice";
 
 function Notifications() {
   const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const markAllAsSeen = async () => {
+    try {
+      dispatch(showLoading());
+      const response = await axios.post(
+        "/api/user/mark-all-notifications-as-seen",
+        {
+          userId: user._id,
+        }
+      );
+      dispatch(hideLoading());
+      if (response.data.success) {
+        toast.success(response.data.message);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      toast.error("Something went wrong.");
+    }
+  };
 
   const items = [
     {
@@ -13,10 +40,15 @@ function Notifications() {
       children: (
         <>
           <div className="d-flex justify-content-end">
-            <h1 className="anchor">Mark all as seen</h1>
+            <h1 className="anchor" onClick={() => markAllAsSeen()}>
+              Mark all as seen
+            </h1>
           </div>
           {user?.unseenNotifications.map((notification) => (
-            <div className="card p-2">
+            <div
+              className="card p-2"
+              onClick={() => navigate(notification.onClickPath)}
+            >
               <div className="card-text">{notification.message}</div>
             </div>
           ))}
