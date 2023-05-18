2860196773b15cb520ef8217260df85a34c55923
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-18 22:48:26 +0200

    Notifications: mark all as seen and delete all

--- client/src/pages/Notifications.js
+++ client/src/pages/Notifications.js
@@ -6,11 +6,14 @@ import toast from "react-hot-toast";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { showLoading, hideLoading } from "../redux/alertsSlice";
+import { setUser } from "../redux/userSlice";
 
 function Notifications() {
   const { user } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  console.log(user?.seenNotifications);
 
   const markAllAsSeen = async () => {
     try {
@@ -19,11 +22,44 @@ function Notifications() {
         "/api/user/mark-all-notifications-as-seen",
         {
           userId: user._id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
         }
       );
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
+        dispatch(setUser(response.data.data));
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      toast.error("Something went wrong.");
+    }
+  };
+
+  const deleteAll = async () => {
+    try {
+      dispatch(showLoading());
+      const response = await axios.post(
+        "/api/user/delete-all-notifications",
+        {
+          userId: user._id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (response.data.success) {
+        toast.success(response.data.message);
+        dispatch(setUser(response.data.data));
       } else {
         toast.error(response.data.message);
       }
@@ -59,9 +95,21 @@ function Notifications() {
       key: "2",
       label: "Seen",
       children: (
-        <div className="d-flex justify-content-end">
-          <h1 className="anchor">Delete all</h1>
-        </div>
+        <>
+          <div className="d-flex justify-content-end">
+            <h1 className="anchor" onClick={() => deleteAll()}>
+              Delete all
+            </h1>
+          </div>
+          {user?.seenNotifications.map((notification) => (
+            <div
+              className="card p-2"
+              onClick={() => navigate(notification.onClickPath)}
+            >
+              <div className="card-text">{notification.message}</div>
+            </div>
+          ))}
+        </>
       ),
     },
   ];
