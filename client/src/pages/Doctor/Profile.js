c8f838a1ca0f077af8d7a8a523d7a4b365bcb44d
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-19 20:02:59 +0200

    Client: added Doctor/Profile

---
+++ client/src/pages/Doctor/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+function Profile() {
+  return (
+    <Layout>
+      <h1 className="page-title">Doctor profile</h1>
+    </Layout>
+  );
+}
+
+export default Profile;
