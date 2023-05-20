2257e4dded89f9ff09c0dd4bb6bab39372daddfc
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-20 15:38:15 +0200

    Client: showing doctorList in antd table

--- client/src/pages/Admin/DoctorList.js
+++ client/src/pages/Admin/DoctorList.js
@@ -1,10 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../../redux/alertsSlice";
+import axios from "axios";
+import { Table } from "antd";
 
 function DoctorList() {
+  const [doctors, setDoctors] = useState([]);
+  const dispatch = useDispatch();
+
+  const getDoctorsData = async () => {
+    try {
+      dispatch(showLoading());
+      const response = await axios.get("/api/admin/get-all-doctors", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      dispatch(hideLoading());
+      if (response.data.success) {
+        setDoctors(response.data.data);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+    }
+  };
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text, record) => (
+        <span>
+          {record.firstName} {record.lastName}
+        </span>
+      ),
+    },
+    {
+      title: "Phone",
+      dataIndex: "phoneNumber",
+    },
+    {
+      title: "Created at",
+      dataIndex: "createdAt",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          {record.status === "pending" && <h1 className="anchor">Approve</h1>}
+          {record.status === "approved" && <h1 className="anchor">Block</h1>}
+        </div>
+      ),
+    },
+  ];
+
   return (
     <Layout>
       <h1 className="page-header">Doctor list</h1>
+      <Table columns={columns} dataSource={doctors} />
     </Layout>
   );
 }
